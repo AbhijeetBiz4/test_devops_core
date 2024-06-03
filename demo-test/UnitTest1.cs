b5fbd4a6667f002c158fa035363c46429cff0a49
Abhijeet Gosavi <abhijeet.gosavi@biz4solutions.com>
2024-06-03 15:12:31 +0530

    initial commit

---
+++ demo-test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace demo_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("123","123");
+        }
+    }
+}
